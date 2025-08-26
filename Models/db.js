0f7fe30ed3bc7324a8ea220c9aca683597f2241c
enedeljkovic <148697260+enedeljkovic@users.noreply.github.com>
2025-08-26 20:09:29 +0200

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,10 +1,16 @@
+
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  'postgresql://postgres.cpyihbhumjsuedrratmr:fdg5ahee@aws-1-eu-central-2.pooler.supabase.com:6543/postgres';
 
-const sequelize = new Sequelize('NurseConnect', 'postgres', 'fdg5ahee', {
-  host: 'localhost',
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
-  logging: false, 
+  logging: false,
+  pool: { max: 15, min: 0, acquire: 30000, idle: 10000 },
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 
 module.exports = sequelize;
